206a1db0e0c145b4c7ccb8d563155af131cf3030
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2024-11-04 09:51:02 +0300

    second screen finished

--- app/src/main/java/com/ideasApp/listofideas/presentation/IdeaItemActivity.kt
+++ app/src/main/java/com/ideasApp/listofideas/presentation/IdeaItemActivity.kt
@@ -29,20 +29,19 @@ class IdeaItemActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         parseIntent()
-
         viewModel = ViewModelProvider(this)[IdeaItemViewModel::class.java]
         setContentView(R.layout.activity_idea_item)
         initViews()
         addTextListeners()
         launchAppropriateMode()
-        addErrorObservers()
+        addObservers()
     }
 
     private fun launchAppropriateMode() {
-        when (screenMode) {
-            MODE_EDIT -> launchEditMode()
-            MODE_ADD  -> launchAddMode()
-        }
+       when(screenMode) {
+           MODE_EDIT -> launchEditMode()
+           MODE_ADD -> launchAddMode()
+       }
     }
 
     private fun addTextListeners() {
@@ -52,41 +51,47 @@ class IdeaItemActivity: AppCompatActivity() {
                 start: Int ,
                 count: Int ,
                 after: Int
-            ) {}
+            ) {
+
+            }
             override fun onTextChanged(s: CharSequence? , start: Int , before: Int , count: Int) {
                 viewModel.resetErrorInputName()
             }
-            override fun afterTextChanged(s: Editable?) {}
+            override fun afterTextChanged(s: Editable?) {
+
+            }
         })
-        editTextName.addTextChangedListener(object: TextWatcher{
+        editTextDescription.addTextChangedListener(object: TextWatcher{
             override fun beforeTextChanged(
                 s: CharSequence? ,
                 start: Int ,
                 count: Int ,
                 after: Int
-            ) {}
+            ) {
+
+            }
             override fun onTextChanged(s: CharSequence? , start: Int , before: Int , count: Int) {
                 viewModel.resetErrorInputDescription()
             }
-            override fun afterTextChanged(s: Editable?) {}
+            override fun afterTextChanged(s: Editable?) {
+
+            }
         })
     }
 
-    private fun addErrorObservers() {
+    private fun addObservers() {
         viewModel.errorInputName.observe(this) {
             val message = if (it) {
                 getString(R.string.error_input_text)
-            }
-            else {
+            } else {
                 null
             }
             inputLayoutName.error = message
         }
         viewModel.errorInputDescription.observe(this) {
             val message = if (it) {
-                getString(R.string.error_input_text)
-            }
-            else {
+                getString(R.string.error_input_description)
+            } else {
                 null
             }
             inputLayoutDescription.error = message
